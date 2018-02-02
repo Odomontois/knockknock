b699e509fc3661b2b47041358503adf3366350e4
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2018-02-02 16:08:43 +0300

    Dotty small progresss

--- src/main/scala/meetup/graphs/Interpret.scala
+++ src/main/scala/meetup/graphs/Interpret.scala
@@ -38,19 +38,17 @@ object Interpret {
     interpret(value => println(s"[${name.value}] $value"))
 
 
-  implicit def consProg[x, y, A, B, C](implicit x: Aux[x, A, B], y: Aux[y, B, C]): Aux[x >> y, A, C] =
-    interpret(input => y.run(x.run(input)))
-
   implicit def comboProg[x, y, Vars <: Record, Out1, Out2]
   (implicit x: Aux[x, Vars, Out1], y: Aux[y, Vars, Out2]): Aux[(x, y), Vars, (Out1, Out2)] =
     interpret(vars => (x.run(vars), y.run(vars)))
 
-
-  def runProg[T, O]()(implicit prog: Aux[T, Unit, O]): Unit = runProgI(())
-  def runProgI[T, I, O](x: I)(implicit prog: Aux[T, I, O]): Unit = prog.run(x)
-
   implicit def varOutputProg[x, Vars <: Record, name <: String, Out, ROut <: Record]
   (implicit x: Aux[x, Vars, Out],
+   update: UpdateRec.Aux[name, Vars, Out, ROut]): Aux[x ->> name, Vars, ROut] =
+    interpret(vars => update(x.run(vars), vars))
+
+  implicit def varOutputProgCons[x, Vars <: Record, name <: String, Out, ROut <: Record]
+  (implicit x: InterpretCons.Aux[x, Vars, Out],
    update: UpdateRec.Aux[name, Vars, Out, ROut]): Aux[x ->> name, Vars, ROut] =
     interpret(vars => update(x.run(vars), vars))
 
@@ -59,16 +57,30 @@ object Interpret {
    x: Aux[x, In, Out]): Aux[x <<- name, Vars, Out] =
     interpret(vars => x.run(select.apply(vars)))
 
+  implicit def varInputProgCons[x, name <: String, In, Out, Vars <: Record]
+  (implicit select: SelectRec.Aux[name, Vars, In],
+   x: InterpretCons.Aux[x, In, Out]): Aux[x <<- name, Vars, Out] =
+    interpret(vars => x.run(select.apply(vars)))
+
   implicit def varInput2Prog[x, name1 <: String, name2 <: String, In1, In2, Out, Vars <: Record]
   (implicit select1: SelectRec.Aux[name1, Vars, In1],
    select2: SelectRec.Aux[name2, Vars, In2],
    x: Aux[x, (In1, In2), Out]): Aux[x <<- (name1, name2), Vars, Out] =
     interpret(vars => x.run((select1(vars), select2(vars))))
 
-  implicit def execProg[prog, Out](implicit prog: Aux[prog, RNil, Out]): Aux[exec[prog], Unit, Unit] =
+  implicit def varInput2ProgCons[x, name1 <: String, name2 <: String, In1, In2, Out, Vars <: Record]
+  (implicit select1: SelectRec.Aux[name1, Vars, In1],
+   select2: SelectRec.Aux[name2, Vars, In2],
+   x: InterpretCons.Aux[x, (In1, In2), Out]): Aux[x <<- (name1, name2), Vars, Out] =
+    interpret(vars => x.run((select1(vars), select2(vars))))
+
+  implicit def execProg[prog, Out](implicit prog: InterpretCons.Aux[prog, RNil, Out]): Aux[exec[prog], Unit, Unit] =
     interpret(_ => {prog.run(RNil); ()})
 
   implicit def doProg[prog, Vars <: Record](implicit prog: AuxI[prog, Vars]): Aux[do_[prog], Vars, Vars] =
+    interpret(vars => {prog.run(vars); vars})
+
+  implicit def doProgCons[prog, Vars <: Record](implicit prog: InterpretCons.AuxI[prog, Vars]): Aux[do_[prog], Vars, Vars] =
     interpret(vars => {prog.run(vars); vars})
 
   implicit def constProg[value <: String, Vars <: Record](implicit valueOf: ValueOf[value]): Aux[const[value], Vars, String] =
@@ -81,6 +93,11 @@ object Interpret {
    expr: Aux[expr, EVars, Out]): Aux[define[pname, ptyp, expr], Vars, ptyp => Out] =
     interpret(vars => param => expr.run(update(param, vars)))
 
+  implicit def defineProgCons[pname, ptyp, expr, Vars <: Record, EVars <: Record, Out]
+  (implicit update: UpdateRec.Aux[pname, Vars, ptyp, EVars],
+   expr: InterpretCons.Aux[expr, EVars, Out]): Aux[define[pname, ptyp, expr], Vars, ptyp => Out] =
+    interpret(vars => param => expr.run(update(param, vars)))
+
   implicit def callProg[I, O] : Aux[call, (I => O, I), O] = interpret((f, x) => f(x))
 
   implicit def getProg[name, Vars <: Record, Out](implicit select: SelectRec.Aux[name, Vars, Out]): Aux[get[name], Vars, Out] =
@@ -88,4 +105,39 @@ object Interpret {
 
   implicit def printVars[Vars <: Record](implicit toList: ToList[Vars]): Aux[printVars, Vars, Vars] =
     interpret(vars => {println(toList(vars)); vars})
+
+
+  def runProg[T, O]()(implicit prog: Aux[T, Unit, O]): Unit = runProgI(())
+  def runProgI[T, I, O](x: I)(implicit prog: Aux[T, I, O]): Unit = prog.run(x)
 }
+
+trait InterpretCons[A]{
+  type Input
+  type Output
+
+  def run: Input => Output
+}
+
+
+object InterpretCons {
+  case class interpret[x, I, O](run: I => O) extends InterpretCons[x] {
+    type Input = I
+    type Output = O
+  }
+
+  type Aux[x, I, O] = InterpretCons[x]{type Input = I; type Output = O}
+  type AuxI[x, I] = InterpretCons[x]{type Input = I}
+
+  implicit def consProg[x, y, A, B, C](implicit x: Interpret.Aux[x, A, B], y: Interpret.Aux[y, B, C]): Aux[x >> y, A, C] =
+    interpret(input => y.run(x.run(input)))
+
+  implicit def consProgR[x, y, A, B, C](implicit x: InterpretCons.Aux[x, A, B], y: Interpret.Aux[y, B, C]): Aux[x >> y, A, C] =
+    interpret(input => y.run(x.run(input)))
+
+  implicit def consProgL[x, y, A, B, C](implicit x: Interpret.Aux[x, A, B], y: InterpretCons.Aux[y, B, C]): Aux[x >> y, A, C] =
+    interpret(input => y.run(x.run(input)))
+
+  implicit def consProgBoth[x, y, A, B, C](implicit x: InterpretCons.Aux[x, A, B], y: InterpretCons.Aux[y, B, C]): Aux[x >> y, A, C] =
+    interpret(input => y.run(x.run(input)))
+}
+
--- src/test/scala/meetup/graphs/prog1.scala
+++ src/test/scala/meetup/graphs/prog1.scala
@@ -8,7 +8,7 @@ import dsl._
 
 object prog1 {
 
-  type Prog1[name <: String] = exec[readLine[name] >> putLine[name]]
+  type Program1[name <: String] = exec[readLine[name] >> putLine[name]]
   type readVar[name <: String] = readLine[name] ->> name
   type putVar[name <: String] =   do_[putLine[name] <<- name]
 
@@ -52,28 +52,31 @@ object prog1 {
 
   type Program5 = exec[
     defun["lollify", "x", String, (const["lol"] ->> "y") >> (concat <<- ("x", "y"))] >>
-//    printVars >>
-    readVar["age"]
-//    do_[apply1["lollify", "age"] >> putLine["name"]]
+    readVar["age"] >>
+    do_[apply1["lollify", "age"] >> putLine["name"]]
   ]
 
   type Program6 = exec[
-//    defun2["concatSpace", "x", String,  "y", String,
-//     ( const["lol"] ->> "sep") >>
-//     ((concat <<- ("x", "sep")) ->> "mid") >>
-//     ( concat <<- ("y", "mid"))] >>
-     readVar["first name"] >>
-//     readVar["last name"]
-//     do_[printVars]
-//    (apply2["concatSpace", "first name", "last name"] ->> "full name") >>
-    putVar["first name"]
+    readVar["first name"] >>
+    readVar["last name"] >>
+    defun2["concatSpace", "x", String,  "y", String,
+     ( const["lol"] ->> "sep") >>
+     ((concat <<- ("x", "sep")) ->> "mid") >>
+     ( concat <<- ("y", "mid"))] >>
+    (apply1["concatSpace", "last name"] ->> "lol")
+//    (apply1["lol", "last name"] ->> "full name")
   ]
 
 
 
   def main(args: Array[String]): Unit = {
+//    runProg[T = Program1["name"]]()
+//    runProg[T = Program2["name"]]()
+//    runProg[T = Program3["name", "age"]]()
+
+
 //    runProg[T = Program4]()
-//    runProg[T = Program5]()
     runProg[T = Program6]()
+//    implicitly
   }
 }
